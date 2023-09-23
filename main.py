f93d664142b1739a681941f299a76a24842f61c2
murilofray <murilofray01@gmail.com>
2023-09-22 23:43:54 -0300

    Adicionando o algoritmo caverphone 2.0

--- main.py
+++ main.py
@@ -1,30 +1,58 @@
 import random
 import sounddevice as sd
 import wavio
-import difflib
+import whisper
+from typing import List
 
 nome_arquivo = "audio.wav"
 idiomas = {'1': 'pt', '2': 'en', '3': 'es'}
 idioma = ""
 
-# Dicionário de palavras e frases para cada nível de dificuldade
 exercicios = {
     'pt': {
-        'facil': ['água', 'sol', 'amor'],
-        'intermediario': ['paralelepípedo', 'hipopotomonstrosesquipedaliofobia', 'anticonstitucionalissimamente'],
-        'dificil': ['inconstitucionalissimamente', 'plenissimamente', 'anticonstitucional']
-    },
-    'en': {
-        'facil': ['water', 'sun', 'love'],
-        'intermediario': ['supercalifragilisticexpialidocious', 'pneumonoultramicroscopicsilicovolcanoconiosis',
-                          'antiestablishmentarianism'],
-        'dificil': ['floccinaucinihilipilification', 'sesquipedalian', 'anticonstitutional']
+        'facil': [
+            'Água', 'Sol', 'Amor', 'Casa', 'Azul', 'Flores', 'Gato', 'Família', 'Trabalho', 'Música',
+            'Felicidade', 'Verão', 'Computador', 'Livro', 'Escola', 'Praia', 'Amizade', 'Coração', 'Chocolate'
+        ],
+        'intermediario': [
+            'Paralelepípedo', 'Procrastinação', 'Imperturbabilidade', 'Indissolubilidade', 'Antissocial',
+            'Inconstitucionalissimamente', 'Esquizofrenia', 'Laboratório',
+        ],
+        'dificil': [
+            'Anticonstitucionalissimamente', 'Excepcional', 'Determinação', 'Palimpsesto',
+            'Eletroencefalograma',
+            'Incompreensibilidade', 'Hipopotomonstrosesquipedaliofobia'
+        ]
     },
     'es': {
-        'facil': ['agua', 'sol', 'amor'],
-        'intermediario': ['hipopotomonstrosesquipedaliofobia', 'supercalifragilisticoespialidoso',
-                          'anticonstitucionalidad'],
-        'dificil': ['neumonoultramicroscopicosilicovolcanoconiosis', 'paralelepipedo', 'anticonstitucional']
+        'facil': [
+            'Agua', 'Sol', 'Amor', 'Casa', 'Azul', 'Flores', 'Gato', 'Familia', 'Trabajo', 'Música',
+            'Felicidad', 'Verano', 'Computadora', 'Libro', 'Escuela', 'Playa', 'Amistad', 'Corazón', 'Chocolate'
+        ],
+        'intermediario': [
+            'Adoquín','Dilación', 'Esquizofrenia', 'Incomprensibilidad'
+        ],
+        'dificil': [
+            'No constitucional', 'ninõ', 'incumbencia', 'cepillo'
+        ]
+    },
+    'en': {
+        'facil': [
+            'Water', 'Sun', 'Love', 'House', 'Blue', 'Flowers', 'Cat', 'Family', 'Work', 'Music',
+            'Happiness', 'Summer', 'Computer', 'Book', 'School', 'Beach', 'Friendship', 'Heart', 'Chocolate'
+        ],
+        'intermediario': [
+            'Supercalifragilisticexpialidocious', 'Pneumonoultramicroscopicsilicovolcanoconiosis',
+            'Antiestablishmentarianism',
+            'Floccinaucinihilipilification', 'Sesquipedalian', 'Anticonstitutional', 'Exceptional', 'Procrastination',
+            'Determination', 'Schizophrenia'
+        ],
+        'dificil': [
+            'Electroencephalographically', 'Incomprehensibilities', 'Antiestablishmentarian', 'Counterdemonstration',
+            'Counterproductiveness',
+            'Counselorship', 'Hipopotomonstrosesquipedaliofobia', 'Paralelepipedo', 'Anticonstitucionalidad',
+            'Neumonoultramicroscopicosilicovolcanoconiosis'
+        ]
     }
 }
 
@@ -61,11 +89,19 @@ def treinamento_diccao():
     print(f"Treinamento de dicção em {idioma} - Nível: {nivel}")
     print(f"Repita a seguinte palavra/frase claramente:")
     print(exercicio)
-
+    print("Aperte enter para começar")
+    input()
     # Gravar a pronúncia do usuário
-    duracao = len(exercicio) * 0.7
+    if len(exercicio) < 5:
+        duracao = len(exercicio) * 0.7
+    elif len(exercicio) < 15:
+        duracao = len(exercicio) * 0.5
+    else:
+        duracao = len(exercicio) * 0.3
+
     gravar_audio(duracao)
     print("Exercício concluído!")
+    print("Aguarde...")
     return exercicio
 
 
@@ -83,46 +119,241 @@ def gravar_audio(duracao):
     wavio.write(nome_arquivo, dados_audio, 44100, sampwidth=3)
 
 
-def calcular_porcentagem_similaridade(str1, str2):
-    def distancia_edicao(str1, str2):
-        len_str1 = len(str1) + 1
-        len_str2 = len(str2) + 1
+def calcular_similaridade_fonetica_en(word1: str, word2: str) -> float:
+    def caverphone2(input: str) -> str:
+        # Implementação do algoritmo Caverphone 2.0 para o inglês
+        __vowels: List[str] = ["a", "e", "i", "o", "u"]
 
-        matriz = [[0] * len_str2 for _ in range(len_str1)]
+        # Step 1.
+        if input.endswith("s"):
+            input = input[:-1]
 
-        for i in range(len_str1):
-            matriz[i][0] = i
+        # Step 2.
+        input = input.replace("ce", "se")
+        input = input.replace("ci", "si")
+        input = input.replace("cy", "sy")
+        input = input.replace("tch", "2ch")
+        input = input.replace("c", "k")
+        input = input.replace("q", "k")
+        input = input.replace("x", "k")
+        input = input.replace("v", "f")
+        input = input.replace("dg", "2g")
+        input = input.replace("tio", "sio")
+        input = input.replace("tia", "sia")
+        input = input.replace("d", "t")
+        input = input.replace("ph", "fh")
+        input = input.replace("b", "p")
+        input = input.replace("sh", "s2")
+        input = input.replace("z", "s")
 
-        for j in range(len_str2):
-            matriz[0][j] = j
+        # Step 3.
+        output = ""
+        for index, char in enumerate(input):
+            if char in __vowels:
+                output += "A" if index == 0 else "3"
+            else:
+                output += char
+        input = output
 
-        for i in range(1, len_str1):
-            for j in range(1, len_str2):
-                custo = 0 if str1[i - 1] == str2[j - 1] else 1
-                matriz[i][j] = min(
-                    matriz[i - 1][j] + 1,  # Deleção
-                    matriz[i][j - 1] + 1,  # Inserção
-                    matriz[i - 1][j - 1] + custo  # Substituição
-                )
+        # Step 4.
+        input = input.replace("j", "y")
 
-        return matriz[len_str1 - 1][len_str2 - 1]
+        # Step 5.
+        if input.startswith("y3"):
+            input = input.replace("y3", "Y3", 1)
+        if input.startswith("y"):
+            input = input.replace("y", "A")
 
-    distancia = distancia_edicao(str1, str2)
-    max_len = max(len(str1), len(str2))
-    similaridade = ((max_len - distancia) / max_len) * 100.0
+        # Step 6.
+        input = input.replace("y", "3")
+
+        # Step 7.
+        input = input.replace("3gh3", "3kh3")
+        input = input.replace("gh", "22")
+        input = input.replace("g", "k")
+
+        # Step 8.
+        input = input.replace("2", "")
+
+        # Step 9.
+        if input.endswith("3"):
+            input = input[:-1] + "A"
+
+        # Step 10.
+        input = input.replace("3", "")
+
+        # Steps 11-12.
+        input = input.ljust(10, "1")
+
+        return input
+
+    code1 = caverphone2(word1)
+    code2 = caverphone2(word2)
+
+    num_caracteres_iguais = sum(1 for a, b in zip(code1, code2) if a == b)
+
+    max_length = max(len(code1), len(code2))
+    similaridade = (num_caracteres_iguais / max_length) * 100.0
 
     return similaridade
+
+
+def calcular_similaridade_fonetica_pt(word1: str, word2: str) -> float:
+    __vowels: List[str] = ["a", "e", "i", "o", "u", "á", "â", "ã", "à", "é", "ê", "í", "ó", "ô", "õ", "ú"]
+
+    def caverphone2_pt(input: str) -> str:
+        input = input.lower()
+        input = ''.join(char for char in input if char.isalpha())
+
+        if input.endswith("e"):
+            input = input[:-1]
+
+        replacements = {
+            "ch": "X",
+            "lh": "L",
+            "nh": "N",
+            "gu": "G",
+            "qu": "Q",
+        }
+        for pattern, replacement in replacements.items():
+            input = input.replace(pattern, replacement)
+
+        output = ""
+        for char in input:
+            if char in __vowels:
+                output += "A"
+            else:
+                output += char
+
+        output = output.replace("b", "B")
+        output = output.replace("c", "C")
+        output = output.replace("d", "D")
+        output = output.replace("f", "F")
+        output = output.replace("g", "G")
+        output = output.replace("h", "H")
+        output = output.replace("j", "J")
+        output = output.replace("k", "K")
+        output = output.replace("l", "L")
+        output = output.replace("m", "M")
+        output = output.replace("n", "N")
+        output = output.replace("p", "P")
+        output = output.replace("q", "Q")
+        output = output.replace("r", "R")
+        output = output.replace("s", "S")
+        output = output.replace("t", "T")
+        output = output.replace("v", "V")
+        output = output.replace("w", "W")
+        output = output.replace("x", "X")
+        output = output.replace("z", "Z")
+
+        output = output.replace("1", "")
+        output = output.replace("3", "")
+        output = output.ljust(6, "1")
+
+        return output
+
+    code1 = caverphone2_pt(word1)
+    code2 = caverphone2_pt(word2)
+
+    num_caracteres_iguais = sum(1 for a, b in zip(code1, code2) if a == b)
+
+    max_length = max(len(code1), len(code2))
+    similaridade = (num_caracteres_iguais / max_length) * 100.0
+
+    return similaridade
+
+
+# Função para calcular similaridade fonética para o Espanhol
+def calcular_similaridade_fonetica_es(word1: str, word2: str) -> float:
+    __vowels: List[str] = ["a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú"]
+
+    def caverphone2_es(input: str) -> str:
+        input = input.lower()
+        input = ''.join(char for char in input if char.isalpha())
+
+        if input.endswith("e"):
+            input = input[:-1]
+
+        replacements = {
+            "ll": "Y",
+            "ñ": "N",
+            "rr": "R",
+            "ch": "X",
+            "gu": "G",
+            "qu": "Q",
+        }
+        for pattern, replacement in replacements.items():
+            input = input.replace(pattern, replacement)
+
+        output = ""
+        for char in input:
+            if char in __vowels:
+                output += "A"
+            else:
+                output += char
+
+        output = output.replace("b", "B")
+        output = output.replace("c", "C")
+        output = output.replace("d", "D")
+        output = output.replace("f", "F")
+        output = output.replace("g", "G")
+        output = output.replace("h", "H")
+        output = output.replace("j", "J")
+        output = output.replace("k", "K")
+        output = output.replace("l", "L")
+        output = output.replace("m", "M")
+        output = output.replace("n", "N")
+        output = output.replace("p", "P")
+        output = output.replace("q", "Q")
+        output = output.replace("r", "R")
+        output = output.replace("s", "S")
+        output = output.replace("t", "T")
+        output = output.replace("v", "V")
+        output = output.replace("w", "W")
+        output = output.replace("x", "X")
+        output = output.replace("z", "Z")
+
+        output = output.replace("1", "")
+        output = output.replace("3", "")
+        output = output.ljust(6, "1")
+
+        return output
+
+    code1 = caverphone2_es(word1)
+    code2 = caverphone2_es(word2)
+
+    num_caracteres_iguais = sum(1 for a, b in zip(code1, code2) if a == b)
+
+    max_length = max(len(code1), len(code2))
+    similaridade = (num_caracteres_iguais / max_length) * 100.0
+
+    return similaridade
+
+
+# Função que recebe as duas palavras e a string do idioma
+def calcular_similaridade_fonetica(word1: str, word2: str, idioma: str) -> float:
+    if idioma == "pt":
+        return calcular_similaridade_fonetica_pt(word1, word2)
+    elif idioma == "en":
+        return calcular_similaridade_fonetica_en(word1, word2)
+    elif idioma == "es":
+        return calcular_similaridade_fonetica_es(word1, word2)
+    else:
+        raise ValueError("Idioma não suportado")
 
 
 if __name__ == "__main__":
     idioma = escolher_idioma()
     while True:
         exercicio = treinamento_diccao()
+        exercicio = exercicio.lower()
         palavraFalada = transcreverAudio()
+        palavraFalada = palavraFalada.lower()
+        palavraFalada = ''.join(char for char in palavraFalada if char.isalnum())
+        porcentagem = calcular_similaridade_fonetica(exercicio, palavraFalada, idioma)
+        print("Palavra falada:", palavraFalada, "Palavra do exercicio:", exercicio)
+        print(f"Porcentagem de igualdade seguindo o algoritmo caverphone2:{porcentagem:.2f}%")
         print("\nDeseja continuar treinando dicção? (s/n)")
         continuar = input()
-        porcentagem = calcular_porcentagem_similaridade(exercicio, palavraFalada)
-        print(f"Porcentagem de dicção': {porcentagem:.2f}%")
         if continuar.lower() != 's':
             break
-
